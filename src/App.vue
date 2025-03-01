96bb2dde3eca5dd61107801627f46580ee06bf4e
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-02-28 23:04:43 -0300

    chore: add main to page content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup></script>
-
 <template>
-    <h1>Hello World</h1>
+    <HeaderNavigation />
+
+    <main id="main">
+        <p v-for="i in [1, 2, 3, 4, 5, 5, 6, 7, 8]" class="container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quisquam nesciunt
+            voluptates mollitia corrupti ab hic ducimus fugiat! Aliquam in veniam dolore
+            reprehenderit quod consectetur eveniet libero corrupti accusantium ea?
+        </p>
+
+        <p class="container--wide">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quaerat ratione
+            praesentium error, reprehenderit aliquam excepturi incidunt dignissimos neque aut,
+            assumenda quod. Tempore quos sed, ea nobis officia saepe minus?
+        </p>
+    </main>
+
+    <FooterNavigation />
 </template>
+
+<script setup>
+import HeaderNavigation from './components/HeaderNavigation.vue'
+import FooterNavigation from './components/FooterNavigation.vue'
+</script>
 
 <style scoped></style>
