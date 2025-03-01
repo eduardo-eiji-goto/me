ed50f220fbe594cdc2e5aa5b81735b6f06849657
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-02-28 23:04:50 -0300

    feat: footer

---
+++ src/components/FooterNavigation.vue
@@ -0,0 +1,96 @@
+<template>
+    <footer id="footer" class="container--wide">
+        <div class="quote" cite="Kung Fu Panda, 2008">
+            <blockquote>
+                Yesterday is history,<br />
+                tomorrow is a mystery,<br />
+                but today is a gift, that is why it is called the present.
+            </blockquote>
+
+            <span>- Oogway</span>
+        </div>
+
+        <div class="flex">
+            <div class="circle-group">
+                <div class="circle text"></div>
+                <div class="circle primary"></div>
+                <div class="circle umber"></div>
+                <div class="circle accent"></div>
+            </div>
+
+            <div class="links">
+                <a
+                    href="https://github.com/eduardo-eiji-goto"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/eduardo-eiji-goto"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    LinkedIn
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+footer {
+    margin-top: 4em;
+    margin-bottom: 2em;
+}
+
+.quote {
+    color: var(--color-umber);
+    border: 2px dashed var(--color-primary);
+    padding: 2em;
+}
+
+blockquote {
+    margin-bottom: 1rem;
+}
+
+.flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1em;
+}
+
+.links {
+    display: flex;
+    gap: 1em;
+}
+
+.circle-group {
+    display: flex;
+    gap: 0.4em;
+}
+
+.circle {
+    height: 1.5em;
+    aspect-ratio: 1;
+}
+
+.text {
+    background: var(--color-text);
+}
+
+.primary {
+    background: var(--color-primary);
+}
+
+.umber {
+    background: var(--color-umber);
+}
+
+.accent {
+    background: var(--color-accent);
+}
+</style>
