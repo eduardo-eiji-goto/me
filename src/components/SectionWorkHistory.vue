f7f4bb254c8d97e8b8e61d35c9657a5e7fd8b2a8
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-03-05 14:49:08 -0300

    chore: section work history

---
+++ src/components/SectionWorkHistory.vue
@@ -0,0 +1,78 @@
+<template>
+    <section class="container">
+        <h2 id="work-history">
+            <a href="#work-history">Work History</a>
+        </h2>
+
+        <section>
+            <h3>
+                <span> <time datetime="2023">2023</time> - <time datetime="2025">2025</time> </span>
+                <br />
+                ExxonMobil
+            </h3>
+
+            <h4>Salesforce Analyst (Sales and Marketing IT)</h4>
+
+            <p>
+                As a Salesforce Analyst and Developer for Exxonmobil, I had the privilege to work in
+                different Salesforce orgs and projects. I developed solutions for both the Fuels and
+                Lubricants Value Chains, for applications using Sales Cloud and Service Cloud
+                resources.
+            </p>
+
+            <p>
+                I brought awareness to the Analyst teams about both Salesforce's Lightning Design
+                System, which should be followed for solutions that are displayed directly in the
+                system, and Developer Tooling, which helps developers write readable quality code.
+            </p>
+
+            <p>
+                By the end of my time in this role, I was the go-to reference for many of the
+                development-related questions our analysts and developers had. From configuration of
+                tools in their machines, which is company specific due to a lot of security
+                controls, to connection to external APIs, either company's owned or from vendors,
+                such as Medallia.
+            </p>
+        </section>
+
+        <section>
+            <h3>
+                <span> <time datetime="2022">2022</time> - <time datetime="2023">2023</time> </span>
+                <br />
+                ExxonMobil
+            </h3>
+
+            <h4>Trainee (DBAM Automation)</h4>
+
+            <p>
+                As a trainee, I worked as a Full Stack Web Developer on a team dedicated to Database
+                Management Automation. The team's main product is a portal offering different
+                resources and applications to DBAs, such as Health Check and Server Mapping.
+            </p>
+
+            <p>
+                Managed a few internal Packages for Web Development, most notably a
+                <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">Vue</a>
+                Component Library based on the company's Design System and a helper for the
+                Sharepoint REST API, which helps a lot with smaller projects (especially those from
+                organizations that lack the development experience).
+            </p>
+
+            <p>
+                I was also part of different initiatives inside the company, helping internal
+                committees with their email communications and web development, both as a developer
+                and consultant.
+            </p>
+        </section>
+    </section>
+</template>
+
+<style scoped>
+h3 > span {
+    font-size: 1rem;
+}
+
+h3:has(span) {
+    line-height: 1;
+}
+</style>
