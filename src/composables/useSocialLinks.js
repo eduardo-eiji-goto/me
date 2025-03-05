7b99978040b4e10e937d0e7174beaf0160cb2c27
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-03-05 12:37:50 -0300

    feat: composable for social links

---
+++ src/composables/useSocialLinks.js
@@ -0,0 +1,12 @@
+export function useSocialLinks() {
+    return [
+        {
+            label: 'Github',
+            to: 'https://github.com/eduardo-eiji-goto',
+        },
+        {
+            label: 'Linkedin',
+            to: 'https://www.linkedin.com/in/eduardo-eiji-goto',
+        },
+    ]
+}
