3c06c21e799ffbef861b114f9fa8cda8ca0e9db3
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-03-05 14:48:59 -0300

    chore: remove h3 links

--- src/components/SectionAboutMe.vue
+++ src/components/SectionAboutMe.vue
@@ -22,47 +22,57 @@
             knowledge to solve problems.
         </p>
 
-        <h3 id="about-me-hobbies">
-            <a href="#about-me-hobbies">Hobbies</a>
-        </h3>
-        <p>
-            My hobbies consist of mostly stay-at-home ones. Reading, programming and baking are some
-            of my favorites. I also love watching movies, either at home or actually going out to
-            the movies for the new releases.
-        </p>
+        <section>
+            <h3>Hobbies</h3>
 
-        <p>
-            Baking has a special place in my heart since it is something I do for other people. I
-            love bringing what I bake to events or work so friends can eat.
-        </p>
+            <p>
+                My hobbies consist of mostly stay-at-home ones. Reading, programming and baking are
+                some of my favorites. I also love watching movies, either at home or actually going
+                out to the movies for the new releases.
+            </p>
 
-        <h3 id="about-me-webdev">
-            <a href="#about-me-webdev">Web Development</a>
-        </h3>
-        <p>
-            As a Web Developer I'm always interested in learning about what the customer wants/needs
-            to communicate, always keeping in touch to make sure the final product feels their own.
-        </p>
+            <p>
+                Baking has a special place in my heart since it is something I do for other people.
+                I love bringing what I bake to events or work so friends can eat.
+            </p>
+        </section>
 
-        <p>
-            For custom solutions I usually work with either React or Vue, and for Salesforce
-            solutions I utilize Salesforce resources, such as Lightning Web Components.
-        </p>
+        <section>
+            <h3>Web Development</h3>
 
-        <h3 id="about-me-salesforce">
-            <a href="#about-me-salesforce">Salesforce</a>
-        </h3>
-        <p>
-            As a Salesforce Analyst, I'm familiar with both Sales Cloud and Service Cloud. I'm able
-            to implement solutions using Flows, Apex Classes, Lightning Web Components and
-            Experience Cloud sites.
-        </p>
+            <p>
+                As a Web Developer I'm always interested in learning about what the customer
+                wants/needs to communicate, always keeping in touch to make sure the final product
+                feels their own.
+            </p>
 
-        <p>
-            My rule of thumb for Salesforce Development is to always search if there are already
-            resources that solve the issue first, only implementing custom solutions if needed. This
-            way I can make sure anyone can work with the application using Salesforce's own
-            documentation.
-        </p>
+            <p>
+                For custom solutions I usually work with either React or Vue, and for Salesforce
+                solutions I utilize Salesforce resources, such as Lightning Web Components.
+            </p>
+        </section>
+
+        <section>
+            <h3>Salesforce</h3>
+
+            <p>
+                As a Salesforce Analyst, I'm familiar with both Sales Cloud and Service Cloud. I'm
+                able to implement solutions using Flows, Apex Classes, Lightning Web Components and
+                Experience Cloud sites.
+            </p>
+
+            <p>
+                My rule of thumb for Salesforce Development is to always search if there are already
+                resources that solve the issue first, only implementing custom solutions if needed.
+                This way I can make sure anyone can work with the application using Salesforce's own
+                documentation.
+            </p>
+        </section>
     </section>
 </template>
+
+<style scoped>
+h3 {
+    margin-top: 1.5rem;
+}
+</style>
