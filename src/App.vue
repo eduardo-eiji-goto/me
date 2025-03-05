b96686b2d1f394e01dc88d24e10b14ff88ded248
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-03-05 14:57:33 -0300

    chore: add sections to app

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,13 @@
     <HeaderNavigation />
 
     <main id="main">
-        <p v-for="i in [1, 2, 3, 4, 5, 5, 6, 7, 8]" class="container">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quisquam nesciunt
-            voluptates mollitia corrupti ab hic ducimus fugiat! Aliquam in veniam dolore
-            reprehenderit quod consectetur eveniet libero corrupti accusantium ea?
-        </p>
+        <SectionIntroduction />
 
-        <p class="container--wide">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quaerat ratione
-            praesentium error, reprehenderit aliquam excepturi incidunt dignissimos neque aut,
-            assumenda quod. Tempore quos sed, ea nobis officia saepe minus?
-        </p>
+        <SectionAboutMe />
+
+        <SectionWorkHistory />
+
+        <SectionEducation />
     </main>
 
     <FooterNavigation />
@@ -21,6 +17,11 @@
 <script setup>
 import HeaderNavigation from './components/HeaderNavigation.vue'
 import FooterNavigation from './components/FooterNavigation.vue'
+
+import SectionIntroduction from './components/SectionIntroduction.vue'
+import SectionAboutMe from './components/SectionAboutMe.vue'
+import SectionWorkHistory from './components/SectionWorkHistory.vue'
+import SectionEducation from './components/SectionEducation.vue'
 </script>
 
 <style scoped></style>
