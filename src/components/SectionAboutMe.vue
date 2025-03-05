3256d135d6931152ae9bb633cce377fa0d40cd31
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-03-05 12:36:47 -0300

    chore: section about me

---
+++ src/components/SectionAboutMe.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="container">
+        <h2 id="about-me">
+            <a href="#about-me">About Me</a>
+        </h2>
+
+        <p>
+            I'm a Salesforce Analyst and Web Developer from
+            <a
+                href="https://en.wikipedia.org/wiki/Curitiba"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Curitiba</a
+            >, where I still reside, a city in Brazil known for its public transportation and
+            <a
+                href="https://revistahaus.com.br/haus/blogs-e-colunistas/key-imaguire-junior/historia-do-petit-pave-as-pedras-portuguesas-das-calcadas/"
+                target="_blank"
+                rel="noopener noreferrer"
+                ><em>Petit Pave</em></a
+            >, beautiful pavement mosaics. I love learning about new technologies and using this
+            knowledge to solve problems.
+        </p>
+
+        <h3 id="about-me-hobbies">
+            <a href="#about-me-hobbies">Hobbies</a>
+        </h3>
+        <p>
+            My hobbies consist of mostly stay-at-home ones. Reading, programming and baking are some
+            of my favorites. I also love watching movies, either at home or actually going out to
+            the movies for the new releases.
+        </p>
+
+        <p>
+            Baking has a special place in my heart since it is something I do for other people. I
+            love bringing what I bake to events or work so friends can eat.
+        </p>
+
+        <h3 id="about-me-webdev">
+            <a href="#about-me-webdev">Web Development</a>
+        </h3>
+        <p>
+            As a Web Developer I'm always interested in learning about what the customer wants/needs
+            to communicate, always keeping in touch to make sure the final product feels their own.
+        </p>
+
+        <p>
+            For custom solutions I usually work with either React or Vue, and for Salesforce
+            solutions I utilize Salesforce resources, such as Lightning Web Components.
+        </p>
+
+        <h3 id="about-me-salesforce">
+            <a href="#about-me-salesforce">Salesforce</a>
+        </h3>
+        <p>
+            As a Salesforce Analyst, I'm familiar with both Sales Cloud and Service Cloud. I'm able
+            to implement solutions using Flows, Apex Classes, Lightning Web Components and
+            Experience Cloud sites.
+        </p>
+
+        <p>
+            My rule of thumb for Salesforce Development is to always search if there are already
+            resources that solve the issue first, only implementing custom solutions if needed. This
+            way I can make sure anyone can work with the application using Salesforce's own
+            documentation.
+        </p>
+    </section>
+</template>
